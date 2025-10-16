78756030927bb929c5683baf6d708b19c84c28cc
akshithaamirishetty <amirishettyakshitha@gmail.com>
2025-10-15 23:32:56 -0700

    Refactor BST implementation with new features

--- bst.c
+++ bst.c
@@ -1,99 +1,147 @@
-#include<stdio.h>
-#include<conio.h>
-#include<malloc.h>
-struct node
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define BST Node
+struct Node
 {
-	int data;
-	struct node *left;
-	struct node *right;
+int key;
+struct Node *left, *right;
 };
-
-struct node *insert(struct node *tree,int val)
+// Create a new node
+struct Node* newNode(int item)
 {
-	struct node *ptr;
-	if(tree==NULL)
-	{
-		ptr=(struct node*)malloc(sizeof(struct node));
-		tree->data=val;
-		tree->left=tree->right=NULL;
-	}
-	else
-	{
-		if(val<tree->data)
-		   insert(tree->left,val);
-		else
-		   insert(tree->right,val);
-	}
-	return tree;
+struct Node* temp = malloc(sizeof(struct Node));
+temp->key = item;
+temp->left = temp->right = NULL;
+return temp;
+}
+// Insert into BST
+struct Node* insertElement(struct Node* root, int key)
+{
+if (root == NULL)
+return newNode(key);
+if (key < root->key)
+root->left = insertElement(root->left, key);
+else if (key > root->key)
+root->right = insertElement(root->right, key);
+return root;
+}
+// Find the node with minimum value (used in deletion)
+struct Node* minValueNode(struct Node* node)
+{
+struct Node* current = node;
+while (current && current->left != NULL)
+current = current->left;
+return current;
 }
 
-void inorder(struct node *tree)
+// Delete a node from BST
+struct Node* deleteNode(struct Node* root, int key)
 {
-	if(tree!=NULL)
-	{
-		inorder(tree->left);
-		printf("%d\t",tree->data);
-		inorder(tree->right);
-	}
+if (root == NULL)
+return root;
+if (key < root->key)
+root->left = deleteNode(root->left, key);
+else if (key > root->key)
+root->right = deleteNode(root->right, key);
+else
+{
+// Case 1: No child
+if (root->left == NULL && root->right == NULL)
+
+{
+free(root);
+return NULL;
 }
-void preorder(struct node *tree)
+// Case 2: One child
+else if (root->left == NULL)
+
 {
-	if(tree!=NULL)
-	{
-		printf("%d\t",tree->data);
-		preorder(tree->left);
-		preorder(tree->right);
-	}
+
+struct Node* temp = root->right;
+free(root);
+return temp;
 }
-void postorder(struct node *tree)
+
+else if (root->right == NULL)
 {
-	if(tree!=NULL)
-	{
-		postorder(tree->left);
-		postorder(tree->right);
-		printf("%d\t",tree->data);
-	}
+
+struct Node* temp = root->left;
+free(root);
+return temp;
 }
-void create_tree(struct node *tree)
+// Case 3: Two children
+struct Node* temp = minValueNode(root->right);
+root->key = temp->key;
+root->right = deleteNode(root->right, temp->key);
+}
+return root;
+}
+
+// Search for an element in BST
+struct Node* searchElement(struct Node* root, int key)
 {
-	tree=NULL;
+if (root == NULL || root->key == key)
+return root;
+if (key < root->key)
+return searchElement(root->left, key);
+else
+return searchElement(root->right, key);
+}
+// Display inorder traversal (to check tree contents)
+void inorderTraversal(struct Node* root)
+{
+if (root)
+{
+inorderTraversal(root->left);
+printf("%d ", root->key);
+inorderTraversal(root->right);
+}
 }
 int main()
 {
-	int option,val;
-	struct node *tree;
-	create_tree(tree);
-	printf("\n\n enter your option:");
-	scanf("%d",&option);
-	while(1)
-	{
-		printf("MAIN MENU");
-		printf("\n1:insert");
-		printf("\n2:inorder");
-		printf("\n3:preorder");
-		printf("\n4:postorder");
-		switch(option)
-		{
-			case 1:
-				printf("enter the value of the node:");
-				scanf("%d",&val);
-				tree=insert(tree,val);
-				break;
-			case 2:
-				printf("\n INORDER:\n");
-				inorder(tree);
-				break;
-			case 3:
-				printf("\n PREORDER:\n");
-				preorder(tree);
-				break;
-			case 4:
-				printf("\n POSTORDER\n");
-				postorder(tree);
-				break;
-				
-		}
-	}
-	getch();
-	return 0;
+struct Node* tree = NULL;
+struct Node* ptr;
+int choice, val;
+while (1)
+{
+printf("\n--- Binary Search Tree Menu ---\n");
+printf("1. Insert\n2. Delete\n3. Search\n4. Display (Inorder)\n5.Exit\n");
+printf("Enter choice: ");
+scanf("%d", &choice);
+
+switch (choice)
+
+{
+case 1:
+printf("\nEnter the value to insert: ");
+scanf("%d", &val);
+tree = insertElement(tree, val);
+break;
+case 2:
+printf("\nEnter the value to delete: ");
+scanf("%d", &val);
+tree = deleteNode(tree, val);
+break;
+case 3:
+printf("\nEnter the value to search: ");
+scanf("%d", &val);
+ptr = searchElement(tree, val);
+if (ptr)
+printf("\nElement %d found in the tree.", val);
+else
+printf("\nElement %d not found in the tree.", val);
+break;
+case 4:
+printf("\nInorder Traversal: ");
+inorderTraversal(tree);
+printf("\n");
+break;
+case 5:
+exit(0);
+default:
+printf("\nInvalid choice!");
+}
+}
+return 0;
 }
